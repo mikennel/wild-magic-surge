93faeeee2b9c5839d62a172432882e1f3e938dc6
Michael Kennel <mikennel@Michaels-MacBook-Pro.local>
2020-01-03 20:38:15 -0500

    surge table language is good now.

--- src/components/routed/Public/Home.vue
+++ src/components/routed/Public/Home.vue
@@ -69,48 +69,48 @@ export default {
         "There is a small explosing directly in front of the caster, covering his or her face in black soot.",
         "For 1 minute any object that is not being worn or carried that the caster touches ignites in flame.",
         "A large geyser of purple water lifts the caster 50 ft in air until start of their next turn.",
-        "Regain lowest-level expended spell slot",
-        "A confused bear appears within 60’ for 1 minute",
-        "For 1 minute, can only shout when speaking",
-        "Grass instantly sprouts to 3’ tall within 20’, 1 minute",
-        "Cast fog cloud centered on self",
-        "Caster can only breathe underwater for next minute",
-        "Up to 3 creatures of choice in 30’:4d10 lightning dmg",
-        "Caster falls Unconscious until start of next turn",
-        "Frightened by nearest creature until end of next turn",
-        "If target dies in next minute, its ghost haunts caster",
-        "Everyone in 30’: invisible for 1 minute or till atk/cast",
-        "All within 60’ saves vs Wis 15 or drops w/e they hold",
-        "Resistance to all damage for next minute",
-        "Everything within 20’ pulled 10’ toward caster",
-        "Random creature within 60’ is poisoned for 24 hours",
-        "Caster smells like lavender for 1d6 days",
-        "Glow brightly for 1 minute. Blind others within 5’.",
-        "Casters clothes become uncomfortably tight",
-        "Cast polymorph on self. Fail save: Sheep form.",
-        "If spell would kill target, target’s extremities fly apart",
-        "Illusory butterflies/flowers flutter in 5’ radius 1 min.",
-        "Caster trapped in a giant glass ball",
-        "Take one additional action immediately",
-        "Caster’s fists become huge, deal 1d8 B dmg, 1 min.",
-        "Others in 30’: 1d10 necro dmg. Gain HP equal to loss",
-        "Loud voice ridicules caster on init count 20 for 1 min",
-        "Cast mirror image",
-        "Caster’s arms become tentacles for 1 minute",
-        "Cast fly on random creature within 60’",
-        "Large floating eye follows caster for 1 hour",
-        "Become invisible/silent for 1 minute or till atk/cast",
-        "Caster’s INT and STR swap for 1 hour",
-        "If you die in next minute, come back per reincarnate",
-        "Caster sees everyone as a decaying corpse for 24h",
-        "Size increases by 1 size category for 1 minute",
-        "All light sources within 60’ radius extinguished",
-        "You and all in 30’: vulnerable to piercing dmg, 1 min.",
-        "Caster becomes frightened of a color for 1 hour",
-        "Surrounded by faint ethereal music for 1 minute",
-        "Caster suffers a head cold for 24 hours",
-        "Regain all expended Sorcery Points",
-        "Next phrase spoken by caster becomes true"]
+        "The caster regains one spell slot of the lowest-level that they have expended.",
+        "A confused bear appears within 60ft of the caster. After 1 minute it disappears.",
+        "For 1 minute, the caster can only shout when speaking.",
+        "All grass within 20ft of the caster instantly sprouts to 3ft tall.",
+        "Cast fog cloud centered on self without expending a spell slot.",
+        "The caster can breathe underwater for 1 minute. However, during this time they are unable to breathe air.",
+        "Up to 3 creatures of the caster's choice within 30ft must make a DC 15 Dexterity saving throw or suffer 4d10 lightning damage.",
+        "The caster falls unconscious until start of their next turn.",
+        "The target is frightened of the nearest creature until end of their next turn.",
+        "If the target dies within the next minute, its ghost haunts the caster.",
+        "Every creature within 30ft of the caster becomes invisible for 1 minute or until they make an attack or cast a spell.",
+        "All creatures within 60ft of the caster must pass a DC 15 Wisdom saving throw or drop whatever they are holding.",
+        "The caster has resistance to all damage for 1 minute.",
+        "All creatures and objects within 20ft of the caster are pulled 10ft closer to him or her.",
+        "A random creature within 60ft is poisoned for 24 hours.",
+        "The caster smells like lavender for 1d6 days.",
+        "The caster glows brightly for 1 minute. Creatures within 5ft are blinded unless they avert their eyes.",
+        "The caster's clothes become uncomfortably tight.",
+        "The caster must make a DC 13 Wisdome saving throw. On a failure they are turned in to a sheep for 1 minute. On a success, they instead choose the form of any beast whose challenge rating is equal to or less than their level.",
+        "If the spell deals damage, the target loses a limb.",
+        "Illusory butterflies and flowers flutter around the caster for 1 min.",
+        "The caster is trapped in a giant glass ball.",
+        "After the spell is cast, the caster immediately takes an additional action.",
+        "Caster’s fists become huge for 1 minute. They are considered a melee weapon with which the caster is proficient. They deal 1d8 bludgeoning damage.",
+        "All other creatures within 30ft suffer 1d10 necrotic dmg. The caster regains HP equal to the total damage caused.",
+        "For one minute a loud voice ridicules the caster at the beginning of each of their turns.",
+        "Cast mirror image without expending a spell slot.",
+        "The caster’s arms become tentacles for 1 minute.",
+        "A random creature within 60ft is the subject of the fly spell.",
+        "A large, floating eye follows caster for 1 hour.",
+        "The caBecome invisible/silent for 1 minute or till atk/cast",
+        "Cast invisibility on yourself without expending a spell slot.",
+        "If the target dies within 1 minute, it immediately comes back to life as per the spell reincarnate.",
+        "The caster sees everyone as a decaying corpse for 24 hours.",
+        "The caster's size increases by 1 size category for 1 minute.",
+        "All light sources within 60ft of the caster are extinguished.",
+        "You and all creatures within 30ft become vulnerable to piercing damage for 1 minute.",
+        "The caster becomes frightened of a color for 1 hour.",
+        "The caster is surrounded by faint, ethereal music for 1 minute.",
+        "The caster suffers a head cold for 24 hours.",
+        "Regain all expended Sorcery Points.",
+        "The next phrase spoken out loud by caster becomes true to some degree."]
     }
   },
   methods: {
@@ -158,7 +158,8 @@ export default {
         .text(
           v-if='hasSurged'
         ) 
-          span {{surgeTable[surgeRoll]}} 
+          h3 {{surgeRoll+1}}
+          p {{surgeTable[surgeRoll]}} 
       .button
         .pro-button.outline.major(
           @click='surge()'
@@ -182,6 +183,10 @@ export default {
           margin: 2em
           border-radius: 1em
           background-color: #ffffff88
+          h3
+            text-align: center
+          p
+            text-align: justify
     .grid, .info, .button
       display: grid
       justify-content: center
