1b8de45269c0c8d0c3bd0437c833cc86f803adb0
Michael Kennel <mikennel@Michaels-MacBook-Pro.local>
2020-01-05 10:29:41 -0500

    added favicon

--- dist/precache-manifest.ed43a9daff60d9348946de5f2c20cd4b.js
+++ dist/precache-manifest.ed43a9daff60d9348946de5f2c20cd4b.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/js/chunk-e9bdc436.b3f559ea.js"
   },
   {
-    "revision": "3f995a10d7de3d951e27",
-    "url": "/js/chunk-4d5def76.b56574a9.js"
+    "revision": "454c61240076df942cc6",
+    "url": "/js/chunk-2ef08401.5d690e4b.js"
   },
   {
-    "revision": "05e25993ac75af278636",
-    "url": "/js/app.0bb4cd88.js"
+    "revision": "ff54db15b3a4f15f6396",
+    "url": "/js/app.f319fbc5.js"
   },
   {
-    "revision": "0934bc8adbe7011c6eb4d4871eee46a6",
+    "revision": "84025c23f95f6f0038d0c83ddccb1b07",
     "url": "/index.html"
   },
   {
@@ -28,11 +28,11 @@ self.__precacheManifest = [
     "url": "/css/chunk-e9bdc436.2d9d7354.css"
   },
   {
-    "revision": "3f995a10d7de3d951e27",
-    "url": "/css/chunk-4d5def76.92320c32.css"
+    "revision": "454c61240076df942cc6",
+    "url": "/css/chunk-2ef08401.5d2aafc5.css"
   },
   {
-    "revision": "05e25993ac75af278636",
+    "revision": "ff54db15b3a4f15f6396",
     "url": "/css/app.c5f166e4.css"
   }
 ];
