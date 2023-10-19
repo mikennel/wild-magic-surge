234e549b974192791d5b8233c49a6f46fdc37924
Michael K <michaelkk42@gmail.com>
2023-10-19 12:41:26 -0400

    refactored to use vite and more up to date node

--- src/main.js
+++ src/main.js
@@ -1,29 +1,9 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
-import firebase from 'firebase'
-import './registerServiceWorker'
 
-export const firebaseApp = firebase.initializeApp(JSON.parse(process.env.VUE_APP_FIREBASE_CONFIG))
+const app = createApp(App)
 
-Vue.use({
-  install: Vue => {
-    Vue.prototype.$firebase = firebaseApp
-  }
-})
+app.use(router)
 
-import VueCookies from 'vue-cookies'
-Vue.use(VueCookies)
-
-import VueResource from 'vue-resource'
-Vue.use(VueResource)
-
-Vue.config.productionTip = false
-
-export const ThisVue = new Vue({
-  el: '#app',
-  router,
-  store,
-  render: function (h) { return h(App) }
-})//.$mount('#app')
+app.mount('#app')
